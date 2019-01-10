30caad2c7b4e4cd2044d2203306b890ec2c4ba15
Mathias Loesch <m.loesch@celonis.com>
2019-01-10 12:41:29 +0100

    yaml/json: dump to sout

--- util/j2y.py
+++ util/j2y.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
-import yaml
 import json
 import sys
-import os
+
+import yaml
 
 
 def main():
     json_path = sys.argv[1]
     with open(json_path) as json_fp:
-        file_name_without_ext = os.path.split(json_fp.name)[-1].split('.')[0]
         d = json.load(json_fp)
-        with open(file_name_without_ext + '.yaml', 'w') as yaml_fp:
-            yaml.dump(d, yaml_fp)
+        print(yaml.dump(d))
 
 
 if __name__ == '__main__':
--- util/y2j.py
+++ util/y2j.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
-import yaml
 import json
 import sys
-import os
+
+import yaml
 
 
 def main():
     yaml_path = sys.argv[1]
     with open(yaml_path) as yaml_fp:
-        file_name_without_ext = os.path.split(yaml_fp.name)[-1].split('.')[0]
         d = yaml.load(yaml_fp)
-        with open(file_name_without_ext + '.json', 'w') as json_fp:
-            json.dump(d, json_fp)
+        print(json.dumps(d))
 
 
 if __name__ == '__main__':
