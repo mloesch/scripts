460a13c6ab396f0cc7464992034ae6d47912d0ce
Mathias Loesch <m.loesch@celonis.com>
2019-01-15 11:48:53 +0100

    j2y: don't use flow style by default

--- util/j2y.py
+++ util/j2y.py
@@ -9,7 +9,7 @@ def main():
     json_path = sys.argv[1]
     with open(json_path) as json_fp:
         d = json.load(json_fp)
-        print(yaml.dump(d))
+        print(yaml.dump(d, default_flow_style=False))
 
 
 if __name__ == '__main__':
